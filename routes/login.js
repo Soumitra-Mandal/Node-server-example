24351f4424b5fc139b9d4df16011ebe2c7aaeb75
soma0721 <soumitra.mandal@netcracker.com>
2023-11-22 17:15:43 +0530

    added auth and data storage using sqlite

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('data/auth.db');
 var error
 router.get('/', (req, res, next) => {
     if (req.session.isLoggedIn) {
@@ -8,15 +10,24 @@ router.get('/', (req, res, next) => {
     error = false;
     res.render('login', { error });
 });
+
 router.post('/', (req, res, next) => {
-    var username = req.body.username
-    var password = req.body.password
-    if (username === "abc" && password === "pass") {
-        req.session.isLoggedIn = true
-        res.redirect("/users");
-    }
-    else {
-        res.render("login", { error: "Invalid Username or Password" })
-    }
+    var username = req.body.username;
+    var password = req.body.password;
+
+    db.get('SELECT * FROM users_auth WHERE name = ?', [username], (err, user) => {
+        if (err) {
+            return res.render("login", { error: err });
+        }
+        if (user == null) {
+            return res.render("login", { error: "User does not exist" });
+        }
+        if (user.password === password) {
+            req.session.isLoggedIn = true;
+            return res.redirect("/users");
+        }
+
+        return res.render("login", { error: "Wrong Password" });
+    });
 });
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,32 +1,36 @@
 var express = require('express');
 var router = express.Router();
-var users = [
-  { "id": "1", "name": "Mary" },
-  { "id": "2", "name": "John" },
-  { "id": "3", "name": "Emily" },
-  { "id": "4", "name": "Alex" },
-  { "id": "5", "name": "Jessica" }
-]
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('data/mydata.db');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  if (req.session.isLoggedIn) {
-    res.json(users)
+  // Check if the user is logged in
+  if (req.session && req.session.isLoggedIn) {
+    // Retrieve users from the database
+    db.all('SELECT * FROM users', (err, data) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+      } else {
+        res.json(data);
+      }
+    });
+  } else {
+    // User is not logged in, render the login page
+    res.render('login', { error: 'Please log in to access users' });
   }
-  else res.render("login", { error: "Please Login to access users" })
-
 });
 
 router.get('/:id', (req, res, next) => {
   if (req.session.isLoggedIn) {
     var id = req.params.id
-    var user = users.find(user => user.id === id)
-    if (user) {
-      res.json(user)
-    }
-    res.send(`<b>Could not find user with id ${id}`)
+    db.get(`SELECT * from users where id=${id}`, (err, data)=>{
+      if(err) {
+        res.status(500).json({ error: err.message });
+      }
+      res.json(data)
+    });
   }
   else res.render("login", { error: "Please Login to access users" })
-
 });
 module.exports = router;
